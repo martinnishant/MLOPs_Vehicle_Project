dba1f3533570611148bd08e098447dd85864526e
Nishant Tomar <nishanttomar514@gmail.com>
2025-08-04 23:54:27 +0530

    Project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Nishant Tomar",
+    author_email="nishanttomar514@gmail.com",
+    packages=find_packages()
+)
